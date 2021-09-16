4b145a6b21f44edfc7a5db109874ddd547f06d83
Nathalia Campos <nathalia@4intelligence.com.br>
2021-09-16 17:10:50 -0300

    feat[POO]: add class and functions

---
+++ POO/main.py
@@ -0,0 +1,16 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+
+# p1.comer('maça')
+# p1.falar('POO')
+# p1.comer('maça')
+# p1.parar_de_comer()
+# p1.parar_de_comer()
+# p1.parar_de_falar()
+# p1.falar('POO')
+# p1.comer('pera')
+# p1.parar_de_falar()
+# p1.comer('pera')
+
+print(p1.get_ano_nascimento())
