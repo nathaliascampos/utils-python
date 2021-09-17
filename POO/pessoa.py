a0ac3fbbdd01fdf38d446e2129f7f6c8ab12863c
Nathalia Campos <nathalia@4intelligence.com.br>
2021-09-17 11:26:22 -0300

    refactor: delete main file

--- POO/pessoa.py
+++ POO/pessoa.py
@@ -63,3 +63,23 @@ class Pessoa:
     @staticmethod
     def gera_id():
         return randint(10000, 19999)
+
+
+# p1 = Pessoa('Luiz', 29)
+
+# p1.comer('maça')
+# p1.falar('POO')
+# p1.comer('maça')
+# p1.parar_de_comer()
+# p1.parar_de_comer()
+# p1.parar_de_falar()
+# p1.falar('POO')
+# p1.comer('pera')
+# p1.parar_de_falar()
+# p1.comer('pera')
+
+p1 = Pessoa.por_ano_nascimento("Maria", 1987)
+print(p1.nome, p1.idade)
+print(p1.get_ano_nascimento())
+
+print(p1.gera_id())
