2a3973b063ec1031609d3e72d3055126a7640ada
Nathalia Campos <nathalia@4intelligence.com.br>
2021-09-17 15:52:59 -0300

    feat[POO]: inheritance

---
+++ POO/example7-pessoa/main.py
@@ -0,0 +1,17 @@
+from classes import Pessoa, Cliente, Aluno
+
+p1 = Pessoa('Maria', 21)
+
+a1 = Aluno('João', 32)
+a1.falar()
+
+c1 = Cliente('João', 32)
+c1.falar()
+c1.comprar()
+
+"""
+Associação -> usa
+Agregação  -> tem
+Composição -> é dono 
+Herança    -> é
+"""
