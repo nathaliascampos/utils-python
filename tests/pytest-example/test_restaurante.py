ff7e983b17aede6d96f474e82c40d03f9d6dfb4e
Nathalia Campos <nathalia@4intelligence.com.br>
2021-09-24 10:11:54 -0300

    refactor: add fixture

--- tests/pytest-example/test_restaurante.py
+++ tests/pytest-example/test_restaurante.py
@@ -3,14 +3,22 @@ import pytest
 from restaurante import Restaurante
 
 
-def test_pedidos_na_fila_valor_inicial_padrao_igual_a_zero():
-    restaurante = Restaurante("Pizzaria X")
-    assert restaurante.pedidos_na_fila == 0
+@pytest.fixture
+def restaurante_fila_vazia():
+    return Restaurante("Pizzaria X")
 
 
-def test_pedidos_na_fila_valor_inicial_maior_que_zero():
-    restaurante = Restaurante("Pizzaria X", 1)
-    assert restaurante.pedidos_na_fila == 1
+@pytest.fixture
+def restaurante_com_um_pedido():
+    return Restaurante("Pizzaria X", 1)
+
+
+def test_pedidos_na_fila_valor_inicial_padrao_igual_a_zero(restaurante_fila_vazia):
+    assert restaurante_fila_vazia.pedidos_na_fila == 0
+
+
+def test_pedidos_na_fila_valor_inicial_maior_que_zero(restaurante_com_um_pedido):
+    assert restaurante_com_um_pedido.pedidos_na_fila == 1
 
 
 def test_pedidos_na_fila_valor_inicial_menor_que_zero():
@@ -18,19 +26,16 @@ def test_pedidos_na_fila_valor_inicial_menor_que_zero():
         Restaurante("Pizzaria X", -1)
 
 
-def test_adiciona_um_pedido_na_fila():
-    restaurante = Restaurante("Pizzaria X", 1)
-    restaurante.adiciona_pedido()
-    assert restaurante.pedidos_na_fila == 2
+def test_adiciona_um_pedido_na_fila(restaurante_com_um_pedido):
+    restaurante_com_um_pedido.adiciona_pedido()
+    assert restaurante_com_um_pedido.pedidos_na_fila == 2
 
 
-def test_remove_um_pedido_na_fila():
-    restaurante = Restaurante("Pizzaria X", 1)
-    restaurante.remove_pedido()
-    assert restaurante.pedidos_na_fila == 0
+def test_remove_um_pedido_na_fila(restaurante_com_um_pedido):
+    restaurante_com_um_pedido.remove_pedido()
+    assert restaurante_com_um_pedido.pedidos_na_fila == 0
 
 
-def test_remove_um_pedido_na_fila_vazia():
-    restaurante = Restaurante("Pizzaria X")
-    restaurante.remove_pedido()
-    assert restaurante.pedidos_na_fila == 0
+def test_remove_um_pedido_na_fila_vazia(restaurante_fila_vazia):
+    restaurante_fila_vazia.remove_pedido()
+    assert restaurante_fila_vazia.pedidos_na_fila == 0
